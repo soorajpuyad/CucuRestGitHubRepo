6fc1b6c450b5c0b8ced03b7167acb9432ada342f
GS-1688 <GS-1688@GS-4382.GSLAB.COM>
2019-09-07 13:09:58 +0530

    My first Commit

---
+++ CucumberRestAssured/src/test/java/com/basic/postStringJsonSD/RunMyPostFromTextFileTest.java
@@ -0,0 +1,29 @@
+package com.basic.postStringJsonSD;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+
+	
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		monochrome=true,
+		dryRun=false,
+				features= {"src/test/resources/com/basic/datatableForGetRequestFF/"},
+				
+				glue={"com/basic/postStringJsonSD/"},
+		plugin={"pretty",
+				"html:target/cucumber-htmlreport",
+				"json:target/cucumber-report774.json",
+				"com.cucumber.listener.ExtentCucumberFormatter:target/ExtentReport1.html"
+		}
+		
+		)
+		
+
+public class RunMyPostFromTextFileTest {
+
+
+}
